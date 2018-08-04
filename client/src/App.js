1410ce45d5a7cb75ef45893efc23dd9f4e91abc8
Rylie Johnson <paintedflame@gmail.com>
2018-08-03 20:20:59 -0500

    adds forms and gets things playing nicely

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import { Input, FormBtn } from "./components/Search";
+import Container from "./components/Grid";
+import API from "./utils/API";
 
 class App extends Component {
+
+  state = {
+    articles: [],
+    title: "",
+    url: "",
+    dateSaved: "",
+    synopsis: ""
+  };
+
+  componentDidMount() {
+    this.loadArticles();
+  }
+
+  loadArticles = () => {
+    API.getArticles()
+      .then(res =>
+        this.setState({ articles: res.data, title: "", url: "", dateSaved: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.term) {
+      API.saveBook({
+        title: this.state.title,
+        author: this.state.author,
+        synopsis: this.state.synopsis
+      })
+        .then(res => this.loadBooks())
+        .catch(err => console.log(err));
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container fluid>
+      <Header />
+      <form>
+        <Input
+          value={this.state.term}
+          onChange={this.handleInputChange}
+          name="term"
+          placeholder="Search Term (required)"
+        />
+        <FormBtn
+          disabled={!(this.state.term)}
+          onClick={this.handleFormSubmit}
+        >
+          Search
+        </FormBtn>
+      </form>
+      </Container>
     );
   }
 }
